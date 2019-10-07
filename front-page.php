f69ec1d3c3fbee753300c5eeea0f9178e8b804fa
Zuieva Inna <zuevainna94@gmail.com>
2019-10-07 16:56:27 -0700

    A tiny change to display the testimonials title dynamically

--- front-page.php
+++ front-page.php
@@ -38,10 +38,46 @@ endwhile; // End of the loop.
 ?>
 
 
+<!------------------------------------------ Deals Slider -------------------------------------->
 
+
+<section class="front-slider slider">
+    <ul class="front-slider slider">
+    <?php
+
+$args = array('post_type' => 'ms-deals', 'posts_per_page' => -1);
+$query = new WP_Query($args);
+if ($query->have_posts()) {
+    echo '<div class="slider">';
+    while ($query->have_posts()) {
+        $query->the_post();
+        the_content();
+    }
+    wp_reset_postdata();
+    echo '</div>';
+}
+?>
+    </section>
+
+<!------------------------------------------ Instagram Feed -------------------------------------->
+
+    <?php echo do_shortcode('[instagram-feed]'); ?>
+
+
+<!------------------------------------------ Testimonials Slider -------------------------------------->
 
 <section class="front-slider">
- <h2 class="testimonials-title">Our customers say</h2>
+
+<?php if (function_exists('get_field')) {
+
+    if (get_field('testimonials')) {
+        echo '<h2 class="testimonials-title">';
+        the_field('testimonials');
+        echo '</h2>';
+    }
+}
+?>
+
 <?php
 
 $args = array('post_type' => 'ms-testimonial', 'posts_per_page' => -1);
