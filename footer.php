34b58dfe7f9131b60d4e0ac85b11c9d37197bb05
Zuieva Inna <zuevainna94@gmail.com>
2019-10-09 14:01:10 -0700

    Registered social media menu for the footer

--- footer.php
+++ footer.php
@@ -15,22 +15,38 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lmpizza' ) ); ?>">
+			<a href="<?php echo esc_url(__('https://wordpress.org/', 'lmpizza')); ?>">
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'lmpizza' ), 'WordPress' );
-				?>
+/* translators: %s: CMS name, i.e. WordPress. */
+// printf( esc_html__( 'Proudly powered by %s', 'lmpizza' ), 'WordPress' );
+?>
 			</a>
-			<span class="sep"> | </span>
+			<span class="sep">  </span>
+
+
+<!-- Social Media Links -->
+
+<nav id="social-navigation" class="social-navigation">
+<?php
+wp_nav_menu(array(
+    'theme_location' => 'social',
+    'menu_id' => 'social-menu',
+));
+?>
+
+</nav><!-- #site-navigation -->
+
 				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'lmpizza' ), 'lmpizza', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+
+echo "Copyright © 2019, All Rights Reserved"
+/* translators: 1: Theme name, 2: Theme author. */
+// printf( esc_html__( 'Theme: %1$s by %2$s.', 'lmpizza' ), 'lmpizza', '<a href="http://underscores.me/">Underscores.me</a>' );
+ ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<?php wp_footer(); ?>
+<?php wp_footer();?>
 
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -48,16 +48,16 @@ endwhile; // End of the loop.
 $args = array('post_type' => 'ms-deals', 'posts_per_page' => -1);
 $query = new WP_Query($args);
 if ($query->have_posts()) {
-    echo '<div class="slider">';
+    echo '<div class="deal-slider slider">';
     while ($query->have_posts()) {
         $query->the_post();
         the_content();
 
         if (function_exists('get_field')) {
 
-            echo '<div class="">';
+            echo '<div class="deal-image">';
             if (get_field('deal_image')) {
-                echo wp_get_attachment_image(get_field('deal_image'), 'full', '', array("class" => "alignleft"));
+                echo wp_get_attachment_image(get_field('deal_image'), 'large', '', array("class" => "alignleft"));
             }
             echo '</div>';
         }
