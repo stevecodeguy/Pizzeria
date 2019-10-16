fed4b0ebd6cc871673a97a5266d3d403799de2c2
Yining Zhang <zyn3607826@gmail.com>
2019-10-16 15:21:20 -0700

    location page style

--- page-location.php
+++ page-location.php
@@ -16,63 +16,79 @@ get_header();
 ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+		
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				get_template_part( 'template-parts/content', 'page' );
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-			// check if the repeater field has rows of data
-			if( have_rows('map') ):
-
-				// loop through the rows of data
-			while ( have_rows('map') ) : the_row();
-
-
-				echo '<h3>' . the_sub_field('location_title') . '</h3>';
-				echo '<p>' . the_sub_field('address') . '</p>';
-				echo '<p>' . the_sub_field('phone') . '</p>';
-				// the_sub_field('hours', );
-				if( have_rows('hours') ):
-					echo '<ul>';
-					while ( have_rows('hours') ) : the_row();
-						echo '<li>Mon: ' . get_sub_field('monday') . '</li>';
-						echo '<li>Tue: ' . get_sub_field('tuesday') . '</li>';
-						echo '<li>Wed: ' . get_sub_field('wednesday') . '</li>';
-						echo '<li>Thurs: ' . get_sub_field('thursday') . '</li>';
-						echo '<li>Fri: ' . get_sub_field('friday') . '</li>';
-						echo '<li>Sat: ' . get_sub_field('saturday') . '</li>';
-						echo '<li>Sun: ' . get_sub_field('sunday') . '</li>';
-					endwhile;
-					echo '</ul>';
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
 				endif;
 
-				// display a sub field value
-				$location = get_sub_field('map_site');
+				// check if the repeater field has rows of data
+				if( have_rows('map') ):
+
+					// loop through the rows of data
+				while ( have_rows('map') ) : the_row();
+				echo '<div class="location-wrapper">';
+					echo '<div class="location-information-wrapper">';
+						echo '<h3>'; 
+							echo the_sub_field('location_title'); 
+						echo '</h3>';
+
+						echo '<p>' ;
+						echo "Address : ";
+							echo  the_sub_field('address');
+						echo '</p>';
+
+						echo '<p class="location-phone-num">';
+							echo "Call : ";
+							echo the_sub_field('phone') ;
+						echo '</p>';
+
+					echo '</div>';
+					// the_sub_field('hours', );
+					if( have_rows('hours') ):
+						echo '<div class="open-hour-wrapper">';
+							echo '<ul class="open-hours">';
+							while ( have_rows('hours') ) : the_row();
+								echo '<li>Mon: ' . get_sub_field('monday') . '</li>';
+								echo '<li>Tue: ' . get_sub_field('tuesday') . '</li>';
+								echo '<li>Wed: ' . get_sub_field('wednesday') . '</li>';
+								echo '<li>Thurs: ' . get_sub_field('thursday') . '</li>';
+								echo '<li>Fri: ' . get_sub_field('friday') . '</li>';
+								echo '<li>Sat: ' . get_sub_field('saturday') . '</li>';
+								echo '<li>Sun: ' . get_sub_field('sunday') . '</li>';
+							endwhile;
+							echo '</ul>';
+						echo'</div>';
+						echo '</div>';//location-wrapper
+					endif;
+
+					// display a sub field value
+					$location = get_sub_field('map_site');
+				
+					if( !empty($location) ):
+						?>
+						<div class="acf-map">
+							<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+						</div>
+						
+					<?php endif;
+		
+				endwhile;
 			
-				if( !empty($location) ):
-					?>
-					<div class="acf-map">
-						<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
-					</div>
-				<?php endif;
+				else :
 
-			endwhile;
+				// no rows found
 
-			else :
-
-			// no rows found
-
-			endif;
-			
-		endwhile; // End of the loop.
-		?>
+				endif;
+				
+			endwhile; // End of the loop.
+			?>
 		
 		</main><!-- #main -->
 	</div><!-- #primary -->
