de5db0d82dc7ec067bc605d707d0762971b3884a
steve990 <steve_990@hotmail.com>
2019-10-16 12:04:33 -0700

    Custom login complete

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,47 @@ function lmpizza_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'lmpizza_pingback_header' );
+
+function lmpizza_login_logo() { 
+	?>
+		<style type="text/css">
+			#login h1 a, .login h1 a {
+				background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo-round.png);
+				background-size: contain;
+				height:200px;
+				width:300px;
+				background-repeat: no-repeat;
+				padding-bottom: 10px;
+			}
+
+			.login p.message{
+				border-left: 4px solid #83253C;
+			}
+
+			.login p.submit input,
+			#wp-submit.button.button-primary.button-large {
+				background: #83253C;
+				border-color: #621c2e;
+				color: #FEE06E;
+				text-shadow: none;
+			}
+
+			input#user_login.input,
+			input#user_pass.input {
+				border-color: #621c2e;
+			}
+
+		</style>
+	<?php 
+	}
+add_action( 'login_enqueue_scripts', 'lmpizza_login_logo' );
+
+function lmpizza_login_logo_url() {
+	return home_url();
+} 
+add_filter( 'login_headerurl', 'lmpizza_login_logo_url'	);
+	
+function lmpizza_login_logo_url_title() {
+	return 'La Migliore Pizzeria';
+} 
+add_filter( 'login_headertitle', 'lmpizza_login_logo_url_title' );
